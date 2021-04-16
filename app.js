27cddcaa0e35669b441bbeeafd239f4d0da92b55
razahamza0308 <razahamza0308@gmail.com>
2021-04-16 03:48:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+var table = prompt("which table you want" )
+
+document.write( "<h1>" + table + "x" +  1 + "=" + table*1 + "<h1>") 
+document.write( "<h1>" + table + "x" +  2 + "=" + table*2 + "<h1>")
+document.write( "<h1>" + table + "x" +  3 + "=" + table*3 + "<h1>")
+document.write( "<h1>" + table + "x" +  4 + "=" + table*4 + "<h1>")
+document.write( "<h1>" + table + "x" +  5 + "=" + table*5 + "<h1>")
+document.write( "<h1>" + table + "x" +  6 + "=" + table*6 + "<h1>")
+document.write( "<h1>" + table + "x" +  7 + "=" + table*7 + "<h1>")
+document.write( "<h1>" + table + "x" +  8 + "=" + table*8 + "<h1>")
+document.write( "<h1>" + table + "x" +  9 + "=" + table*9 + "<h1>")
+document.write( "<h1>" + table + "x" +  10 + "=" + table*10 + "<h1>")
